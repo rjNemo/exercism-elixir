bcc4a9f0764ed5c2a0600592cfecc11709308331
Ruidy <rnemausat@newstore.com>
2022-03-12 11:08:07 -0400

    anagram

--- anagram/lib/anagram.ex
+++ anagram/lib/anagram.ex
@@ -3,6 +3,13 @@ defmodule Anagram do
   Returns all candidates that are anagrams of, but not equal to, 'base'.
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
-  def match(base, candidates) do
-  end
+  def match(base, candidates),
+    do:
+      Enum.filter(
+        candidates,
+        &(String.downcase(&1) != String.downcase(base) and
+            to_graphemes(&1) == to_graphemes(base))
+      )
+
+  defp to_graphemes(string), do: String.downcase(string) |> String.graphemes() |> Enum.sort()
 end
